5ad7582a1ce84b71dc55f4d3b837b6796868d564
egadestaviano <pastilaku121@gmail.com>
2025-08-11 14:55:37 +0700

    Fix Ui 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="flex justify-center items-center h-screen bg-gradient-to-br from-primary-50 to-primary-100">
       <div className="text-center p-4">
-        <h1 className="text-4xl font-bold text-red-500">404</h1>
+        <h1 className="text-4xl font-bold text-primary-600">404</h1>
         <p className="text-xl text-gray-700">
           Oops! The page you&apos;re looking for could not be found.
         </p>
         <div className="mt-4">
           <Link
             href="/"
-            className="text-blue-500 hover:underline"
+            className="text-primary-600 hover:underline"
           >
             Go back to Home
           </Link>
